1e05d5dc35adf0d957d93487a98c3527479fa57a
Damjan96 <jovanovicdam@gmail.com>
2019-07-19 15:21:12 +0200

    First attempt at client to client chatting.

--- solo_client_threaded.py
+++ solo_client_threaded.py
@@ -10,6 +10,8 @@ class StandaloneClient():
         self.host = host
         self.context = zmq.Context.instance()
 
+    # This part opens up the sockets for receiving messages and loops the
+    # new message check, that will be improved later.
     def listener(self):
         sub_socket = self.context.socket(zmq.SUB)
         connection = "tcp://{}:{}".format(self.host, self.sub_port)
@@ -19,6 +21,9 @@ class StandaloneClient():
             message = sub_socket.recv_json()
             print(message)
 
+    # This part starts a separate thread that activates the receiving part of
+    # the client, enabling messages to come while the user is inputing a new
+    # message.
     def sender(self):
         listen_thread = Thread(target=self.listener)
         listen_thread.daemon = True
@@ -29,7 +34,8 @@ class StandaloneClient():
             msg = input()
             senders.send_json(msg)
 
-
+    # Should be replaced with a better variety of setup options.
+    # Currently based on manual input for both sides.
 if __name__ == '__main__':
     interface = '*'
     sub_port = '5555'

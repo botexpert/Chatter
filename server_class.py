e6d945132898a3273ddbc6bc436c22e5043251d5
botexpert <botexpert@localhost.localdomain>
2019-07-24 13:59:07 +0200

    Working server? 	modified:   server_class.py

--- server_class.py
+++ server_class.py
@@ -1,40 +1,52 @@
 import zmq
 from threading import Thread
 import time
+import json
 
 class Server:
     def __init__(self,address,rcv_port,snd_port):
         self.address=address
         self.recv_port=rcv_port
-        self.send_port=snd_port
         self.contex=zmq.Context.instance()
         self.recv_socket=None
-        self.send_socket=None
 
     def server_bind (self):
         self.recv_socket = self.contex.socket(zmq.ROUTER)
         bind_address_rcv = 'tcp://{}:{}'.format(self.address,self.recv_port)
         self.recv_socket.bind(bind_address_rcv)
-
-        self.send_socket = self.contex.socket(zmq.ROUTER)
-        bind_address_snd = 'tcp://{}:{}'.format(self.address,self.send_port)
-        self.send_socket.bind(bind_address_snd)
+        print('Socket Binded')
 
     def recieve_message(self):
-        data = self.send_socket.recv_json()
+        data = self.recv_socket.recv_json(zmq.NOBLOCK)
         ID = data['id']
+        TO = data['to']
         message = data['message']
-        return ID,message
+        print('{}sent to {}: {}'.format(ID,TO,message))
+        return ID,TO,message
 
-    def send_message (self,client_id,client_message):
-        self.send_socket.send(b'{}sent: {}' % client_id %client_message)
+    def send_message (self,client_id,client_to,client_message):
+        data={'id':client_id,
+              'to':client_to,
+              'message':client_message}
+        self.recv_socket.send_json(data,zmq.NOBLOCK,routing_id=client_to)
 
     def server_run(self):
         self.server_bind()
-        receive = Thread(target=self.recieve_message())
-        receive.daemon = True
-        receive.start()
+        poller=zmq.Poller()
+        poller.register(self.recv_socket,zmq.POLLIN)
+        print('socket Registered in poller')
+        #receive = Thread(target=self.recieve_message())
+        #receive.daemon = True
+        #receive.start()
         while True:
-            if self.recv_socket.poll(1):
-                ID,new_message = self.recieve_message()
-                self.send_message(ID,new_message)
+            print('in while 1')
+            events = dict(poller.poll(timeout=250))
+            print('events done')
+            while True:
+                if self.recv_socket in events:
+                    ID,TO,new_message = self.recieve_message()
+                    self.send_message(ID,TO,new_message)
+                    print('message sent')
+                else:
+                    print('no messages')
+                    break

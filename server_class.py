8a79961c8f9967bee752355068350ca0a6cc508f
botexpert <botexpert@localhost.localdomain>
2019-07-24 23:28:51 +0200

    Server working finally 	modified:   server_class.py 	modified:   server_init.py

--- server_class.py
+++ server_class.py
@@ -4,32 +4,36 @@ import time
 import json
 
 class Server:
-    def __init__(self,address,rcv_port,snd_port):
+    def __init__(self,address,rcv_port,server_id):
         self.address=address
         self.recv_port=rcv_port
+        self.server_id=server_id.encode()
         self.contex=zmq.Context.instance()
         self.recv_socket=None
 
     def server_bind (self):
         self.recv_socket = self.contex.socket(zmq.ROUTER)
-        self.recv_socket.setsockopt(zmq.IDENTITY,b'serverID')
+        self.recv_socket.setsockopt(zmq.IDENTITY,self.server_id)
         bind_address_rcv = 'tcp://{}:{}'.format(self.address,self.recv_port)
         self.recv_socket.bind(bind_address_rcv)
         print('Socket Binded')
 
     def recieve_message(self):
-        data = self.recv_socket.recv_json(zmq.NOBLOCK)
-        ID = data['id']
+        ID,data_raw = self.recv_socket.recv_multipart()
+        data=json.loads(data_raw)
         TO = data['to']
         message = data['message']
         print('{}sent to {}: {}'.format(ID,TO,message))
-        return ID,TO,message
+        return ID.decode('utf-8'),TO.encode(),message
+
 
     def send_message (self,client_id,client_to,client_message):
         data={'id':client_id,
-              'to':client_to,
               'message':client_message}
-        self.recv_socket.send_json(data,zmq.NOBLOCK,routing_id=client_to)
+        s=json.dumps(data).encode()
+        send_data=[client_to,s]
+        self.recv_socket.send_multipart(send_data)
+        '''routing_id=client_to'''
 
     def server_run(self):
         self.server_bind()
@@ -47,8 +51,10 @@ class Server:
                 print('in while 2')
                 if self.recv_socket in events:
                     ID,TO,new_message = self.recieve_message()
+                    print('message received')
                     self.send_message(ID,TO,new_message)
                     print('message sent')
+                    break
                 else:
                     print('no messages')
                     break
--- server_init.py
+++ server_init.py
@@ -1,4 +1,4 @@
 from server_class import Server
 
-server = Server('127.0.0.1',5555,5556)
+server = Server('127.0.0.1',5555,'serverID')
 server.server_run()

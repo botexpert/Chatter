b3629a7dba4e790f6b547bdde633f10487c73892
botexpert <botexpert@localhost.localdomain>
2019-07-29 13:35:50 +0200

    Added soe comments to code, for understatnding 	modified:   server_class.py 	modified:   server_init.py

--- server_class.py
+++ server_class.py
@@ -3,6 +3,8 @@ from threading import Thread
 import time
 import json
 
+#Server for communication using ZMQ library
+
 class Server:
     def __init__(self,address,rcv_port,server_id):
         self.address=address
@@ -11,6 +13,7 @@ class Server:
         self.contex=zmq.Context.instance()
         self.recv_socket=None
 
+#Bind socket to address
     def server_bind (self):
         self.recv_socket = self.contex.socket(zmq.ROUTER)
         self.recv_socket.setsockopt(zmq.IDENTITY,self.server_id)
@@ -18,6 +21,7 @@ class Server:
         self.recv_socket.bind(bind_address_rcv)
         print('Socket Binded')
 
+#Recieve raw message and process it
     def recieve_message(self):
         ID,data_raw = self.recv_socket.recv_multipart()
         data=json.loads(data_raw)
@@ -26,7 +30,7 @@ class Server:
         print('{}sent to {}: {}'.format(ID,TO,message))
         return ID.decode('utf-8'),TO.encode(),message
 
-
+#Send Json class but encoded to bytes
     def send_message (self,client_id,client_to,client_message):
         data={'id':client_id,
               'message':client_message}
@@ -35,26 +39,25 @@ class Server:
         self.recv_socket.send_multipart(send_data)
         '''routing_id=client_to'''
 
+#Start server
     def server_run(self):
         self.server_bind()
         poller=zmq.Poller()
         poller.register(self.recv_socket,zmq.POLLIN)
         print('socket Registered in poller')
-        #receive = Thread(target=self.recieve_message())
-        #receive.daemon = True
-        #receive.start()
+
+        #Check for events on socket
         while True:
-            print('in while 1')
+
             events = dict(poller.poll(timeout=250))
-            print('events done')
+
             while True:
-                print('in while 2')
+                #If message is received, process it and send it to target
                 if self.recv_socket in events:
                     ID,TO,new_message = self.recieve_message()
-                    print('message received')
+                    print('Message received: {}sent to{} :{}'.format(ID,TO,new_message))
                     self.send_message(ID,TO,new_message)
-                    print('message sent')
+                    print('Message sent to {}'.format(TO))
                     break
                 else:
-                    print('no messages')
                     break
